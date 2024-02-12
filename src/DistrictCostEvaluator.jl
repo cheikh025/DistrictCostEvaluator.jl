ec93a9d873e95d16fb5d45218533d1eaa0d16be8
cheikh025 <cheikh.aghibou@gmail.com>
2024-02-12 13:12:12 -0500

    update

--- src/DistrictCostEvaluator.jl
+++ src/DistrictCostEvaluator.jl
@@ -1,5 +1,22 @@
 module DistrictCostEvaluator
 
-# Write your package code here.
+    using CxxWrap
+    # Define a function that returns the library path
+    function get_libGenSC_path()
+        script_dir = @__DIR__
+        return joinpath(script_dir, "../deps/cpp/build/libCostEvaluator.so")
+    end
+    script_dir = @__DIR__
+    @wrapmodule(joinpath(script_dir, "../deps/cpp/build/libCostEvaluator.so"))
+    
+    function __init__()
+        @initcxx
+    end
+    
+    # Define a Julia function that calls the C++ function
+    function evaluate_cost(districts, instance_name)
+        return @cxx DistrictCostEvaluator.EVmain(districts, instance_name)
+    end
 
+    export evaluate_cost
 end
